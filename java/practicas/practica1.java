a80d1fd2512f4f20894ef56a99a229c15ef49568
joseinve <116449272+joseinve@users.noreply.github.com>
2023-03-04 16:30:51 -0300

    .

--- java/practicas/practica1.java
+++ java/practicas/practica1.java
@@ -1,5 +1,4 @@
 package practicas;
-
 // Declara dos variables numéricas (con el valor que desees), muestra por consola la suma, resta, multiplicación, división y módulo (resto de la división).
 public class practica1 {
     public static void main(String[] args) {
