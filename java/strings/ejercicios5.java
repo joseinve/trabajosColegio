fccb151bbebab5e5443f4a816bc32c174216dda8
joseinve <116449272+joseinve@users.noreply.github.com>
2023-03-04 15:14:50 -0300

    .

--- java/strings/ejercicios5.java
+++ java/strings/ejercicios5.java
@@ -1,6 +1,19 @@
 package strings;
 
+import java.util.Scanner;
+
 public class ejercicios5 {
 	public static void main(String[] args) {
+		Scanner cadena1= new Scanner(System.in);
+		String cadena_1 = cadena1.nextLine();
+		Scanner cadena2= new Scanner(System.in);
+		String cadena_2 = cadena2.nextLine();
+			String[] cadena3 = cadena_2.split(" ");
+			for (String cadena : cadena3) {
+				cadena_1 = cadena_1.replaceAll(cadena,cadena.toUpperCase());
+			}
+		System.out.println(cadena_1); 
+		cadena1.close();
+		cadena2.close();
 	}
 }
