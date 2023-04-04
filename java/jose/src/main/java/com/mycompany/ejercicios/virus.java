91c54670a80226f783058531c1f3bf0db939fa71
joseinve <116449272+joseinve@users.noreply.github.com>
2023-04-04 13:18:37 -0400

    .

---
+++ java/jose/src/main/java/com/mycompany/ejercicios/virus.java
@@ -0,0 +1,25 @@
+package com.mycompany.ejercicios;
+
+public class virus {
+    int madre;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void inicializar() {
+
+    }
+
+    public static void madre(int madre) {
+
+    }
+
+    public static void maxgen() {
+
+    }
+
+    public static void originaria(int cepas) {
+
+    }
+}
