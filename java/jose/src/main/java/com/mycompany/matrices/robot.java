743df5f468e27a6e0002824892e55bf22d278ec2
joseinve <116449272+joseinve@users.noreply.github.com>
2023-05-06 17:57:47 -0400

    .

--- java/jose/src/main/java/com/mycompany/matrices/robot.java
+++ java/jose/src/main/java/com/mycompany/matrices/robot.java
@@ -6,15 +6,64 @@ import java.util.Scanner;
 public class robot {
     static int x = 0, y = 0;
 
-    public static void Robot(int n1, int m1, int y, int x, ArrayList comandos) {
-switch (	) {
-    case value:
-        
-        break;
+    public static void Robot(int n1, int m1, int y, int x, ArrayList comandos, String o) {
+        int num;
+        switch (o.toUpperCase()) {
+            case "N":
 
-    default:
-        break;
-}
+                break;
+            case "S":
+
+                break;
+            case "E":
+
+                break;
+            case "O":
+
+                break;
+        }
+        for (int i = 0; i < comandos.size(); i += 2) {
+            switch (comandos.get(i + 1).toString()) {
+                case "0":
+                    num = 0;
+                    break;
+                case "1":
+                    num = 1;
+                    break;
+                case "2":
+                    num = 2;
+                    break;
+                case "3":
+                    num = 3;
+                    break;
+                case "4":
+                    num = 4;
+                    break;
+                case "5":
+                    num = 5;
+                    break;
+                case "6":
+                    num = 6;
+                    break;
+                case "7":
+                    num = 7;
+                    break;
+                case "8":
+                    num = 8;
+                    break;
+                case "9":
+                    num = 9;
+                    break;
+            }
+            switch (comandos.get(i).toString()) {
+                case "a":
+
+                    break;
+                case "r":
+
+                    break;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -28,8 +77,13 @@ switch (	) {
         y = n.nextInt();
         System.out.print("Ingrese la columna inicial del robot:");
         x = m.nextInt();
+        Scanner o = new Scanner(System.in);
+        String orientacion = o.nextLine();
         Scanner comando = new Scanner(System.in);
         ArrayList comandos = new ArrayList();
+        for (int i = 0; i < args.length; i++) {
+
+        }
         System.out.println("Introduzca los comandos a seguir");
         String com = comando.nextLine().toLowerCase();
         while (!com.contains("salir")) {
@@ -49,7 +103,7 @@ switch (	) {
                 break;
             }
         }
-        Robot(n1, m1, y, x, comandos);
+        Robot(n1, m1, y, x, comandos, orientacion);
         n.close();
         comando.close();
         m.close();
