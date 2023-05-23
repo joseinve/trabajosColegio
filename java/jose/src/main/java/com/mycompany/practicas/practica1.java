e809348edb347b082173dbbcdcb8787312d642ca
joseinve <116449272+joseinve@users.noreply.github.com>
2023-05-23 15:05:41 -0400

    .

--- java/jose/src/main/java/com/mycompany/practicas/practica1.java
+++ java/jose/src/main/java/com/mycompany/practicas/practica1.java
@@ -1,9 +1,22 @@
 package com.mycompany.practicas;
+
 // Declara dos variables numéricas (con el valor que desees), muestra por consola la suma, resta, multiplicación, división y módulo (resto de la división).
 public class practica1 {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static double sum(int a, Double b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        int b=5;
-        int a=2;
-        System.out.println("La suma de los dos elementos es "+(a+b)+", la resta de estos es "+(a-b)+" ,el producto es "+(a*b)+", la división es "+(a/b)+" y el modulo es "+(a%b));
+        int b = 5;
+        int a = 2;
+        double c = 3.4;
+        sum(a, c);
+        sum(a, b);
+        System.out.println("La suma de los dos elementos es " + (a + b) + ", la resta de estos es " + (a - b)
+                + " ,el producto es " + (a * b) + ", la división es " + (a / b) + " y el modulo es " + (a % b));
     }
 }
